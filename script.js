7124ea3127a1a4cb3155c00a09cb8948a0737ba9
vcolavin <vcolavin@gmail.com>
2017-03-04 22:53:32 -0800

    make text draggable

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var myCodeMirror = CodeMirror.fromTextArea(myTextArea, {
   viewportMargin: Infinity
 })
 
+$('.input-area').draggable()
+
 $('#render-button').click(function(e) {
   e.preventDefault()
   window.eval(myCodeMirror.getValue())

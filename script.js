3eaf0708d488030b91f87eef04ce6a8cc66d0a6e
vcolavin <vcolavin@gmail.com>
2017-03-04 22:37:02 -0800

    continue work installing codemirror

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-$('#render-button').click(function() {
-  console.log('ya clicked me!')
+var myTextArea = document.getElementById('main-input')
+var myCodeMirror = CodeMirror.fromTextArea(myTextArea, {
+  value: 'this is the starting value',
+  viewportMargin: Infinity
 })
+
+$('#render-button').click(function(e) {
+  e.preventDefault()
+})
+
